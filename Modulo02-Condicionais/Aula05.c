3275c84cfe5dcd7cbd3b90c13fd1e77abd2981f7
Arthur <kimadzn@gmail.com>
2023-11-12 00:40:43 -0300

    .

--- Modulo02-Condicionais/Aula05.c
+++ Modulo02-Condicionais/Aula05.c
@@ -1,23 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-
-
-int main (void) {
-
-    int nota1, nota2, nota3, media;
-
-    printf("Escreva 3 notas para o aluno!\n"); 
-    scanf("%d %d %d", &nota1, &nota2, &nota3);
-    media = (nota1+nota2+nota3)/3;
-
-    if(media>= 7)
-    {
-        printf("Aluno APROVADO!!");
-    }else
-    {
-        printf("Aluno REPROVADO!");
+ 
+//Função principal do programa
+void main(){
+ 
+    //Definindo Variáveis
+    int a = 18;
+ 
+    //Conectivo Lógico E (AND) , Se UMA COMPARACAO for FALSA, Não entra no bloco
+    if(a > 5 && a > 15){
+        printf("\n A variavel 'a' e maior que 5 e 15 ");
     }
-
-    return 0;
+ 
+    //Conectivo Lógico OU (OR) , Se UMA COMPARACAO for VERDADEIRA, já entra no bloco
+    if(a > 5 || a > 15){
+        printf("\n A variavel 'a' eh maior que 5 ou 15 ");
+    }
+ 
+    //Misturando Conectivos
+    if( (a > 5 && a < 15) || a == 20 ){
+        printf("\n A variavel 'a' esta entre 5 e 15 ou ela vale 20 ");
+    }
+ 
+ 
 }
