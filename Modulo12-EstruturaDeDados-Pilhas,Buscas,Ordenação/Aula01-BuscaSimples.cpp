697ff751c9580fdf72d742d11a54b35473b61137
Arthur <kimadzn@gmail.com>
2023-11-23 20:49:22 -0300

    .

---
+++ Modulo12-EstruturaDeDados-Pilhas,Buscas,Ordenação/Aula01-BuscaSimples.cpp
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+#include <iostream>
+#include <new>
+
+
+using namespace std;
+
+
+int main (void)
+{
+
+
+    int vetor [10] = {12, 2, 41, 2,13, 21, 21, 421, 44, 22};
+    int valorProcurado; 
+    int posicao;
+
+    for (int i = 0; i < 10; i++)
+    {
+        cout << vetor[i] << " - "; 
+    }
+    
+
+    printf("Qual numero deseja encontrar no vet");
+    scanf("%d", &valorProcurado);
+
+    for (int i = 0; i < 11; i++)
+    {
+        
+    }
+    
+
+
+    return 0;
+}
